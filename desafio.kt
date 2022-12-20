de1859c0e72f1f73b049bc4daf42559f1c71c8b1
joaovsbuarque <joaobuarqueestudos@gmail.com>
2022-12-20 16:22:03 -0300

    desafio kt commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Level { BASIC, INTERMEDIARY, ADVANCED }
+enum class Post { STUDENT, TEACHER }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Users(val fNome: String, val lNome: String, val email: String, val post: Post)
 
-class Usuario
+data class EducationalContent(val name: String, val duration: Int, val level: Level)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formation(val nome: String, val contents: List<EducationalContent>, val level: Level) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    val registered = mutableListOf<Users>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun addUser(user: Users) {
+         registered.add(user)
+    }
+
+    fun getAddUser(){
+        for(user in registered){
+            println("|Nome: ${user.fNome+" "+user.lNome}| - |Email: ${user.email}| - (${user.post})")
+        }
+    }
+
+    fun printEducationalContent(){
+        for(cont in contents){
+            println("${cont.name} - ${cont.duration}min - (${cont.level})")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ 	val moduleKotlinB = EducationalContent("Linguagem Kotlin", 120, Level.BASIC)
+    val moduleAndroidStudioB = EducationalContent("Fundamentos Android Studio", 120, Level.BASIC)
+  	val moduleKotlinI = EducationalContent("Linguagem Kotlin", 180, Level.INTERMEDIARY)
+  	val moduleAndroidStudioI = EducationalContent("Fundamentos Multiscreen", 180, Level.INTERMEDIARY)
+
+	val contentList = mutableListOf<EducationalContent>()
+  		contentList.add(moduleKotlinB)
+  		contentList.add(moduleAndroidStudioB)
+    	contentList.add(moduleKotlinI)
+  		contentList.add(moduleAndroidStudioI)
+
+    val luan = Users("Luan","Moraes", "luanmoraes@gmail.com", Post.STUDENT)
+    val camili = Users("Camili","Compagnoni", "camilicompagnoni@gmail.com", Post.STUDENT)
+    val venilton = Users("Venilto","Falvo", "veniltonfalvo@gmail.com", Post.TEACHER)
+
+    val androidTraining = Formation("Formação Android Developer", contentList, Level.INTERMEDIARY)
+
+    androidTraining.addUser(luan)
+    androidTraining.addUser(camili)
+    androidTraining.addUser(venilton)
+
+  	println(" -----------------------------------------")
+    println("|${androidTraining.nome} - ${androidTraining.level}|")
+    println(" -----------------------------------------")
+
+    println("Grade do curso")
+    println(androidTraining.printEducationalContent())
+    println("******************************************")
+
+    println("Usuarios cadastrados no curso")
+    println(androidTraining.getAddUser())
 }
